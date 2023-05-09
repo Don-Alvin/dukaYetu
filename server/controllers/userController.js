07c4d6d240ac94b877e1b8b03e04f2b3c1bcd3c5
Don Alvin <alvindon41@gmail.com>
2023-05-09 14:47:06 +0300

    Update password

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -134,6 +134,24 @@ exports.getUserProfile = catchAsyncErrors(async (req, res, next) => {
 	});
 });
 
+// Update / Change Password = > /api.v1/password/update
+exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
+	const user = await User.findById(req.user.id).select("+password");
+
+	// Check previous user password
+
+	const isMatched = await user.comparePassword(req.body.oldPassword);
+
+	if (!isMatched) {
+		return next(new ErrorHandler("Old password is incorrect"));
+	}
+
+	user.password = req.body.password;
+	await user.save();
+
+	sendToken(user, 200, res);
+});
+
 // Logout => /api/v1/logout
 exports.logoutUser = catchAsyncErrors(async (req, res, next) => {
 	res.cookie("token", null, {
