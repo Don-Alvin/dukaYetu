b916d620285867fb38e43da0a9accefd9271d3a3
Don Alvin <alvindon41@gmail.com>
2023-05-10 12:12:10 +0300

    get all orders

--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -1,9 +1,19 @@
 const { Router } = require("express");
 const router = Router();
 
-const { newOrder } = require("../controllers/orderControllers");
-const { isAuthenticatedUser } = require("../middlewares/auth");
+const {
+	newOrder,
+	getMyOrders,
+	getSingleOrder,
+	getAllOrders,
+} = require("../controllers/orderControllers");
+const { isAuthenticatedUser, authorizedRoles } = require("../middlewares/auth");
 
 router.route("/order/new").post(isAuthenticatedUser, newOrder);
+router.route("/orders/me").get(isAuthenticatedUser, getMyOrders);
+router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
+router
+	.route("/admin/orders")
+	.get(isAuthenticatedUser, authorizedRoles("admin"), getAllOrders);
 
 module.exports = router;
